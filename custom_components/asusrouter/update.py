fb41fd6e6627d513eb89b05a1c820da523a24711
Yevhenii Vaskivskyi <yevhenii@vaskivskyi.com>
2024-03-20 22:38:03 +0100

    0.30.0 (#812)
    * Add `color_mode` property for LED entity (#808)
    
    * Add `check_for_updates` button (#809)
    
    * Add support for a full release note in the update entity (#810)
    
    * Add update installation (#811)
    
    * Bump asusrouter to `1.8.0`
    
    * Bump version to `0.30.0`

--- custom_components/asusrouter/update.py
+++ custom_components/asusrouter/update.py
@@ -2,7 +2,12 @@
 
 from __future__ import annotations
 
-from homeassistant.components.update import UpdateEntity
+import asyncio
+import logging
+from typing import Any
+
+from asusrouter.modules.system import AsusSystem
+from homeassistant.components.update import UpdateEntity, UpdateEntityFeature
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
@@ -12,6 +17,8 @@ from .const import STATIC_UPDATES
 from .dataclass import ARUpdateDescription
 from .entity import ARBinaryEntity, async_setup_ar_entry
 from .router import ARDevice
+
+_LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(
@@ -45,3 +52,40 @@ class ARUpdate(ARBinaryEntity, UpdateEntity):
         self._attr_installed_version = self.extra_state_attributes.get("current")
         self._attr_latest_version = self.extra_state_attributes.get("available")
         self._attr_release_summary = self.extra_state_attributes.get("release_note")
+        self._attr_in_progress = False
+
+        self._attr_supported_features = (
+            UpdateEntityFeature.RELEASE_NOTES
+            | UpdateEntityFeature.INSTALL
+            | UpdateEntityFeature.PROGRESS
+        )
+
+    def release_notes(self) -> str | None:
+        """Return the release notes."""
+
+        return self.extra_state_attributes.get("release_note")
+
+    async def async_install(
+        self, version: str | None, backup: bool, **kwargs: Any
+    ) -> None:
+        """Install the update."""
+        try:
+            _LOGGER.debug(
+                "Trying to install Firmware update. This might take several minutes."
+            )
+            result = await self.api.async_set_state(
+                state=AsusSystem.FIRMWARE_UPGRADE,
+            )
+            if not result:
+                _LOGGER.debug(
+                    "Something went wrong while trying to install the update."
+                )
+            else:
+                self._attr_in_progress = True
+                await asyncio.sleep(120)
+                self._attr_in_progress = False
+
+        except Exception as ex:  # pylint: disable=broad-except
+            _LOGGER.error(
+                "An exception occurred while trying to install the update: %s", ex
+            )
