3f14535e624fdbd5a6dd89f34a1d5104d596100f
Yevhenii Vaskivskyi <yevhenii@vaskivskyi.com>
2025-09-12 05:24:03 +0200

    Roll back from using webpanel property

--- custom_components/asusrouter/bridge.py
+++ custom_components/asusrouter/bridge.py
@@ -253,7 +253,17 @@ class ARBridge:
         self._identity = identity
 
         # Set properties
-        self._configuration_url = self.api.webpanel
+        _cfg_url_port = self._configs.get(CONF_PORT, None)
+        if not _cfg_url_port:
+            _cfg_url_port = (
+                DEFAULT_PORT_HTTPS
+                if self._configs[CONF_SSL]
+                else DEFAULT_PORT_HTTP
+            )
+        self._configuration_url = (
+            f"{"https" if self._configs[CONF_SSL] else "http"}://"
+            f"{self._host}:{_cfg_url_port}"
+        )
         self._identifiers = set()
         if identity.mac is not None:
             self._identifiers.add((DOMAIN, format_mac(identity.mac)))
