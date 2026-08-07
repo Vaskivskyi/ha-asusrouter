84c2dac44e8e2fbdecfd991f78a639b4cd652e2c
Jeremiah J. Gassensmith <jgassens@gmail.com>
2026-08-07 16:42:42 -0500

    Satisfy Home Assistant release validation

--- custom_components/asusrouter/__init__.py
+++ custom_components/asusrouter/__init__.py
@@ -7,6 +7,7 @@ import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import EVENT_HOMEASSISTANT_STOP
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.device_registry import DeviceEntry
 from homeassistant.helpers.typing import ConfigType
 
@@ -15,6 +16,8 @@ from .router import ARDevice
 from .services import async_setup_services
 
 _LOGGER = logging.getLogger(__name__)
+
+CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
 
 
 async def async_setup(
